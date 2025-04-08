825a306dbc9f3024f542ecb287f970ad14baa228
rrryyykkk <rezkymubarok0256@gmail.com>
2025-04-08 17:15:43 +0700

    penambahan di logoPt dulu

--- fe/src/components/LandingPage/LogoPartner.jsx
+++ fe/src/components/LandingPage/LogoPartner.jsx
@@ -3,18 +3,29 @@ import { Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/autoplay";
 
-const logos = ["/1.png", "/2.png", "/3.png", "/4.png"];
+const logos = [
+  "/logoPT/amazon.png",
+  "/logoPT/google.png",
+  "/logoPT/ibm.png",
+  "/logoPT/apple.png",
+  "/logoPT/pertamina.png",
+  "/logoPT/sony.png",
+  "/logoPT/meta.png",
+  "/logoPT/mi.jpg",
+  "/logoPT/microsoft.jpg",
+  "/logoPT/philips.webp",
+];
 
 const LogoPartner = () => {
   return (
-    <section className="py-12 bg-indigo-950">
-      <h2 className="text-center text-2xl font-bold mb-6 text-white">
+    <section className="py-12 bg-indigo-300">
+      <h2 className="text-center text-2xl font-bold mb-6 text-black">
         Didukung Oleh
       </h2>
       <Swiper
         modules={[Autoplay]}
         spaceBetween={30}
-        slidesPerView={2}
+        slidesPerView={4}
         autoplay={{
           delay: 2000,
           disableOnInteraction: false,
